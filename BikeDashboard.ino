43d4352bf5a929f460a351031e522d4f413d9fdb
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2021-04-18 20:38:39 +0300

    refactorized code and added update loop

--- BikeDashboard.ino
+++ BikeDashboard.ino
@@ -6,45 +6,11 @@
     - bec stins => 1 logic
     - puls: tranzitie 1 -> 0
 */
-
-#include <LiquidCrystal.h>
-
-#define LCD_PIN_RS 12
-#define LCD_PIN_EN 11
-#define LCD_PIN_D4 4
-#define LCD_PIN_D5 5
-#define LCD_PIN_D6 6
-#define LCD_PIN_D7 7
-
-#define LCD_COLS 16
-#define LCD_ROWS 2
-
-#define LCD_ROW_VAL_SPEED 0
-#define LCD_COL_VAL_SPEED 0
-#define LCD_ROW_TXT_SPEED 0
-#define LCD_COL_TXT_SPEED 4
-
-#define LCD_ROW_VAL_TEMP 0
-#define LCD_COL_VAL_TEMP 10
-#define LCD_ROW_TXT_TEMP 0
-#define LCD_COL_TXT_TEMP 14
-
-#define LCD_ROW_VAL_DISTANCE 1
-#define LCD_COL_VAL_DISTANCE 0
-#define LCD_ROW_VAL_DISTANCE_TOTAL 1
-#define LCD_COL_VAL_DISTANCE_TOTAL 6
-#define LCD_ROW_TXT_DISTANCE 1
-#define LCD_COL_TXT_DISTANCE 12
-
-#define MOVING_AVERAGE_WINDOW_SIZE 2
-#define DIFF_RATIO_THRESHOLD 10
-
 typedef unsigned long ulong;
-typedef unsigned int uint;
 
 struct Node
 {
-    uint data;
+    ulong data;
     Node *next;
 };
 
@@ -105,70 +71,105 @@ public:
         float average = this->sum / ((float)this->current_size);
         return average;
     }
-    void show_data()
-    {
-        Serial.print(" ( ");
-        for(Node *p=this->head; p != NULL; p=p->next)
-        {
-            Serial.print(p->data);
-            Serial.print(' ');
-        }
-        Serial.print(") ");
-    }
 };
+
+#include <LiquidCrystal.h>
+
+#define LCD_PIN_RS 12
+#define LCD_PIN_EN 11
+#define LCD_PIN_D4 4
+#define LCD_PIN_D5 5
+#define LCD_PIN_D6 6
+#define LCD_PIN_D7 7
+
+#define LCD_COLS 16
+#define LCD_ROWS 2
+
+#define LCD_ROW_VAL_SPEED 0
+#define LCD_COL_VAL_SPEED 0
+#define LCD_ROW_TXT_SPEED 0
+#define LCD_COL_TXT_SPEED 4
+
+#define LCD_ROW_VAL_TEMP 0
+#define LCD_COL_VAL_TEMP 10
+#define LCD_ROW_TXT_TEMP 0
+#define LCD_COL_TXT_TEMP 14
+
+#define LCD_ROW_VAL_DISTANCE 1
+#define LCD_COL_VAL_DISTANCE 0
+#define LCD_ROW_VAL_DISTANCE_TOTAL 1
+#define LCD_COL_VAL_DISTANCE_TOTAL 6
+#define LCD_ROW_TXT_DISTANCE 1
+#define LCD_COL_TXT_DISTANCE 12
+
+#define IR_PIN 2 // pin 2 has interrupt 0
+#define DEFAULT_BAUD_RATE 9600
+#define UPDATE_INTERVAL_MS 1000
+#define MOVING_AVERAGE_WINDOW_SIZE 2
+#define PULSE_RATIO_THRESHOLD 10
+#define WHEEL_LENGTH_CM 235
+#define CONST_SPEED_KMH (3600000 * WHEEL_LENGTH_CM / 100 / 1000) // = 8460
+//#define CONST_CM_TO_KM 1000000
 
 char line[17];
 LiquidCrystal LCD(LCD_PIN_RS, LCD_PIN_EN, LCD_PIN_D4, LCD_PIN_D5, LCD_PIN_D6, LCD_PIN_D7);
 volatile MovingAverage movingAverage(MOVING_AVERAGE_WINDOW_SIZE);
-volatile long current_millis, last_time_millis, last_diff_millis, diff_millis, diff_ratio, false_pulses_count;
+volatile ulong current_pulse_millis, pulse_last_time_millis, pulse_last_diff_millis, diff_pulse_millis, diff_pulse_ratio, pulse_count_invalids;
 volatile float average_diff, speed_kmh, distance_covered, rpm;
+ulong update_current_millis, update_last_millis;
 
 void setup()
 {  
-    Serial.begin(9600);
+    Serial.begin(DEFAULT_BAUD_RATE);
 
     pinMode(2, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(2), ISR_count_IR_pulses, FALLING);
-    last_time_millis = millis();
-    last_diff_millis = 0;
-    false_pulses_count = 0;
+    attachInterrupt(digitalPinToInterrupt(IR_PIN ), ISR_count_IR_pulses, FALLING);
+    pulse_last_time_millis = millis();
+    pulse_last_diff_millis = 0;
+    pulse_count_invalids = 0;
+    update_last_millis = millis();
     delay(100);
 }
 
 void loop()
 {
-    Serial.print(rpm);
-    Serial.print(' ');
-    Serial.print(speed_kmh);
-    Serial.print(' ');
-    Serial.println(distance_covered);
+    update_current_millis = millis();
+    if(update_current_millis - update_last_millis > UPDATE_INTERVAL_MS)
+    {
+        update_last_millis = update_current_millis;
+        Serial.print(rpm);
+        Serial.print(' ');
+        Serial.print(speed_kmh);
+        Serial.print(' ');
+        Serial.println(distance_covered);
+    }
 }
 
 void ISR_count_IR_pulses()
 {
-    current_millis = millis();
-    diff_millis = current_millis - last_time_millis;
-    diff_ratio = max(last_diff_millis, diff_millis) / min(last_diff_millis, diff_millis);
-    if(diff_ratio > DIFF_RATIO_THRESHOLD) // filter out
+    current_pulse_millis = millis();
+    diff_pulse_millis = current_pulse_millis - pulse_last_time_millis;
+    diff_pulse_ratio = max(pulse_last_diff_millis, diff_pulse_millis) / min(pulse_last_diff_millis, diff_pulse_millis);
+    if(diff_pulse_ratio > PULSE_RATIO_THRESHOLD) // filter out
     {
-        ++false_pulses_count;
-        if(false_pulses_count == 3)
+        ++pulse_count_invalids;
+        if(pulse_count_invalids == 3)
         {
-            false_pulses_count = 0; // reset this counter and reset the state
-            last_diff_millis = 0; // shold have set it to zero, but above i might have division by zero
-            last_time_millis = current_millis;
+            pulse_count_invalids = 0; // reset this counter and reset the state
+            pulse_last_diff_millis = 0; // shold have set it to zero, but above i might have division by zero
+            pulse_last_time_millis = current_pulse_millis;
         }
     }
     else // keep current value
     {
-        movingAverage.add_data(diff_millis);
-        false_pulses_count = 0;
-        last_diff_millis = diff_millis;
-        last_time_millis = current_millis;
+        movingAverage.add_data(diff_pulse_millis);
+        pulse_count_invalids = 0;
+        pulse_last_diff_millis = diff_pulse_millis;
+        pulse_last_time_millis = current_pulse_millis;
         average_diff = movingAverage.compute_average();
-        distance_covered += 235; // wheel length in cm
-        speed_kmh = 8460 / average_diff;
-        rpm = 60000 / average_diff;
+        distance_covered += WHEEL_LENGTH_CM; // wheel length in cm
+        speed_kmh = CONST_SPEED_KMH / average_diff;
+        //rpm = 60000 / average_diff;
         /*
             Formula for speed:
                 average  ms ... 2.35 m
@@ -196,20 +197,4 @@ void lcd_display(int col, int row, float value, int total_length, int n_decimals
         dtostrf(value, total_length, n_decimals, line);
         LCD.print(line);
     }
-}
-void temp_code()
-{
-    /*LCD.begin(16, 2);
-    LCD.setCursor(0, 0);
-    LCD.print("R=");
-    LCD.setCursor(6, 0);
-    LCD.print("F=");
-    LCD.setCursor(0, 1);
-    LCD.print("RP=");
-    LCD.setCursor(9, 1);
-    LCD.print("TM=");
-    lcd_display(2, 0, AmountOfReadings, 3, 0);
-    lcd_display(9, 0, FrequencyReal, 6, 0);
-    lcd_display(3, 1, RPM, 5, 0);  
-    lcd_display(12, 1, average, 4, 0);*/
 }
