ab1e7bf12b419cb7cf1083bedc5146a7cf55aded
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2021-08-20 18:17:51 +0300

    added second screen in arduino

--- BikeDashboard.ino
+++ BikeDashboard.ino
@@ -10,6 +10,21 @@
     Arduino Nano settings:
     - Board: Arduino Nano
     - Processor: ATMega328P simple
+
+    LCD menu:
+    1) speed, crt dist, total dist, temperature
+    2) moving time, max speed (km/h), avg speed (kmh), avg pace (time/km)
+
+    Formula for speed:
+        average  ms ... 2.35 m
+        3600 000 ms ... x km/h !!!
+        x = speed(km/h) = (3 600 000 ms * 2.35 m) / average ms  / 1000 = 8460 / average
+    Formula for distance travelled:
+        add wheel length at each revolution
+    Formula for var_rpm:
+        average ms ... 1 rot
+         60 000 ms ... x rot
+         x = var_rpm = 60 000 ms * rot / average ms
 */
 
 #include <LiquidCrystal_I2C.h>
@@ -17,16 +32,123 @@
 #include "DHT.h"
 
 typedef unsigned long ulong;
+#define LCD_COLS 20
+#define LCD_ROWS 4
+#define LCD_I2C_ADDRESS 0x27
 
-struct Node
+// DISPLAY 1
+#define LCD_ROW_TXT_SPEED 0 // the "Speed(km/h):" text
+#define LCD_COL_TXT_SPEED 0
+#define LCD_ROW_VAL_SPEED 0 // speed value
+#define LCD_COL_VAL_SPEED 15
+
+#define LCD_ROW_TXT_DISTANCE_CRT 1 // the "Crt dist:" text
+#define LCD_COL_TXT_DISTANCE_CRT 0
+#define LCD_ROW_VAL_DISTANCE_CRT 1 // the current distance value
+#define LCD_COL_VAL_DISTANCE_CRT 15
+
+#define LCD_ROW_TXT_DISTANCE_TOTAL 2 // the "Total dist:" text
+#define LCD_COL_TXT_DISTANCE_TOTAL 0
+#define LCD_ROW_VAL_DISTANCE_TOTAL 2 // the total distance value
+#define LCD_COL_VAL_DISTANCE_TOTAL 14
+
+#define LCD_ROW_TXT_TEMPERATURE 3 // the "Temp. (*C):" text
+#define LCD_COL_TXT_TEMPERATURE 0
+#define LCD_ROW_VAL_TEMPERATURE 3 // the temp value
+#define LCD_COL_VAL_TEMPERATURE 15
+
+// DISPLAY 2
+#define LCD_ROW_TXT_TIME_ELAPSED 0  // the "TimeElapsed:" text
+#define LCD_COL_TXT_TIME_ELAPSED 0
+#define LCD_ROW_VAL_TIME_ELAPSED 0  // the time elapsed value
+#define LCD_COL_VAL_TIME_ELAPSED 12
+
+#define LCD_ROW_TXT_MAX_SPEED 1  // the "MaxSpeed(km/h):" text
+#define LCD_COL_TXT_MAX_SPEED 0
+#define LCD_ROW_VAL_MAX_SPEED 1  // the max speed value
+#define LCD_COL_VAL_MAX_SPEED 15
+
+#define LCD_ROW_TXT_AVG_SPEED 2  // the "AvgSpeed(km/h):" text
+#define LCD_COL_TXT_AVG_SPEED 0
+#define LCD_ROW_VAL_AVG_SPEED 2  // the avg speed value
+#define LCD_COL_VAL_AVG_SPEED 15
+
+#define LCD_ROW_TXT_AVG_PACE 3  // the "AvgPace(t/km):" text
+#define LCD_COL_TXT_AVG_PACE 0
+#define LCD_ROW_VAL_AVG_PACE 3  // the avg pace value
+#define LCD_COL_VAL_AVG_PACE 14
+
+// TEMPERATURE SENSOR
+#define DHT11_PIN 10
+#define DHT11_TYPE DHT11
+
+// HALL SEOSOR
+#define HALL_PIN 2 // pin #2 has interrupt 0 (INT0)
+
+// PUSH BUTTON
+#define RESET_DISTANCE_BUTTON_PIN 3 // pin #3 has interrupt 1 (INT1), but I use digitalRead
+#define DISPLAY_NEXT_SCREEN_MS 50
+#define RESET_CRT_DIST_TIMEOUT_MS 3000
+#define RESET_TOTAL_DIST_TIMEOUT_MS 5000
+
+#define DEFAULT_BAUD_RATE 9600
+#define UPDATE_INTERVAL_MS 1000
+#define MOVING_AVERAGE_WINDOW_SIZE 2
+#define PULSE_RATIO_THRESHOLD 10
+#define WHEEL_LENGTH_CM 235
+#define CONST_SPEED_KMH (3600000 * WHEEL_LENGTH_CM / 100 / 1000) // = 8460
+#define CONST_MAX_INVALID_PULSES 3
+#define CONST_ZERORIZE_SPEED_THRESHOLD_MS 3000 // set it to high value to get smaller speeds
+#define MAX_LCD_DISPLAYS 2
+
+char line[17];
+char time_string[12]; // "10h35m43s#"
+int display_length;
+byte symbol_celsius_degree[8] = { B01110, B10001, B10001, B01110, B00000, B00000, B00000 };
+volatile long pulse_current_millis = 0, pulse_last_time_millis = 0;
+volatile long pulse_last_diff_millis = 0, pulse_diff_millis = 0, diff_pulse_ratio = 0, pulse_count_invalids = 0;
+volatile long var_distance_temp = 0, var_distance_total = 0, var_rpm = 0;
+volatile float average_diff = 0, var_speed_kmh = 0;
+float var_max_speed_kmh = 0, var_avg_speed_kmh = 0;
+ulong var_avg_pace_skm = 0;
+ulong total_moving_time_secs = 0;
+ulong update_current_millis = 0, update_last_millis = 0, button_last_time_high = 0, button_time_diff = 0;
+ulong button_last_value = 0; // use this to avoid screen bouncing
+
+LiquidCrystal_I2C LCD(LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS);
+DHT dht(DHT11_PIN, DHT11_TYPE);
+
+class Average
 {
-    ulong data;
-    Node *next;
+private:
+    float sum;
+    ulong count;
+public:
+    Average()
+    {
+        this->sum = 0;
+        this->count = 0;
+    }
+
+    void add(float val)
+    {
+        this->sum += val;
+        ++(this->count);
+    }
+
+    float compute_average()
+    {
+        if(this->count == 0) return 0;
+        float avg = this->sum / this->count;
+        return avg;
+    }
 };
+Average avgSpeed;
 
 class MovingAverage
 {
 private:
+    struct Node { ulong data; Node *next; };
     ulong window_size, sum, current_size;
     Node *head, *tail;
 public:
@@ -37,9 +159,9 @@ public:
         this->sum = 0;
         this->head = this->tail = NULL;
     }
-    void add_data(ulong data)
+    
+    void add(ulong data)
     {
-        // whatever we run below, add the data to the sum
         this->sum += data;
         if(this->current_size == 0)
         {
@@ -64,7 +186,7 @@ public:
             this->sum -= this->head->data;
             // set node to head
             Node *node = this->head;
-            // move head to the second node
+            // move head to the next node
             this->head = this->head->next;
             // replace first node's data with new data
             node->data = data;
@@ -76,11 +198,14 @@ public:
             this->tail = node;
         }
     }
+    
     float compute_average()
     {
+        if(this->current_size == 0) return(float) 0;
         float average = this->sum / ((float)this->current_size);
         return average;
     }
+    
     void show_data()
     {
         Serial.print(" ( ");
@@ -92,7 +217,7 @@ public:
         Serial.print(") ");
     }
 };
-
+volatile MovingAverage movingAverage(MOVING_AVERAGE_WINDOW_SIZE);
 
 class MemoryHandler
 {
@@ -128,6 +253,7 @@ private:
         }
         return dist_cm;
     }
+    
 public:
     static int const MODE_TOTAL_DIST=0, MODE_CRT_DIST=1;
 
@@ -174,93 +300,186 @@ public:
         return (ulong) 0;
     }
 };
-
-/* LCD DEFINES */
-//#define LCD_PIN_RS 12
-//#define LCD_PIN_EN 11
-//#define LCD_PIN_D4 4
-//#define LCD_PIN_D5 5
-//#define LCD_PIN_D6 6
-//#define LCD_PIN_D7 7
-
-#define LCD_COLS 20
-#define LCD_ROWS 4
-#define LCD_I2C_ADDRESS 0x27
-
-#define LCD_ROW_TXT_SPEED 0 // the "Speed(km/h):" text
-#define LCD_COL_TXT_SPEED 0
-#define LCD_ROW_VAL_SPEED 0 // speed value
-#define LCD_COL_VAL_SPEED 15
-
-#define LCD_ROW_TXT_DISTANCE_CRT 1 // the "Crt dist:" text
-#define LCD_COL_TXT_DISTANCE_CRT 0
-#define LCD_ROW_VAL_DISTANCE_CRT 1 // the current distance value
-#define LCD_COL_VAL_DISTANCE_CRT 15
-
-#define LCD_ROW_TXT_DISTANCE_TOTAL 2 // the "Total dist:" text
-#define LCD_COL_TXT_DISTANCE_TOTAL 0
-#define LCD_ROW_VAL_DISTANCE_TOTAL 2 // the total distance value
-#define LCD_COL_VAL_DISTANCE_TOTAL 14
-
-#define LCD_ROW_TXT_TEMP 3 // the "Temp. (*C):" text
-#define LCD_COL_TXT_TEMP 0
-#define LCD_ROW_VAL_TEMP 3 // the temp value
-#define LCD_COL_VAL_TEMP 15
-
-/* LCD DEFINES */
-#define DHT11_PIN 10
-#define DHT11_TYPE DHT11
-
-#define HALL_PIN 2 // pin #2 has interrupt 0 (INT0)
-#define RESET_DISTANCE_BUTTON_PIN 3 // pin #3 has interrupt 1 (INT1), but I use digitalRead
-#define RESET_CRT_DIST_TIMEOUT_MS 3000
-#define RESET_TOTAL_DIST_TIMEOUT_MS 5000
-
-#define DEFAULT_BAUD_RATE 9600
-#define UPDATE_INTERVAL_MS 1000
-#define MOVING_AVERAGE_WINDOW_SIZE 2
-#define PULSE_RATIO_THRESHOLD 10
-#define WHEEL_LENGTH_CM 235
-#define CONST_SPEED_KMH (3600000 * WHEEL_LENGTH_CM / 100 / 1000) // = 8460
-#define CONST_MAX_INVALID_PULSES 3
-#define CONST_ZERORIZE_SPEED_THRESHOLD_MS 3000
-
-
-char line[17];
-int display_length;
-byte symbol_celsius_degree[8] = { B01110, B10001, B10001, B01110, B00000, B00000, B00000 };
-volatile MovingAverage movingAverage(MOVING_AVERAGE_WINDOW_SIZE);
-volatile long pulse_current_millis, pulse_last_time_millis;
-volatile long pulse_last_diff_millis, pulse_diff_millis, diff_pulse_ratio, pulse_count_invalids;
-volatile long var_distance_temp, var_distance_total, var_rpm;
-volatile float average_diff, var_speed_kmh;
-ulong update_current_millis, update_last_millis, button_last_time, button_time_diff;
-
-LiquidCrystal_I2C LCD(LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS);
-DHT dht(DHT11_PIN, DHT11_TYPE);
 MemoryHandler mem;
+
+class TimeClock
+{
+private:
+    byte hh, mm, ss;
+    ulong total_seconds;
+public:
+    TimeClock()
+    {
+        this->reset();
+    }
+
+    void reset()
+    {
+        this->hh = 0;
+        this->mm = 0;
+        this->ss = 0;
+        this->total_seconds = 0;
+    }
+    
+    void tick()
+    {
+        ++(this->total_seconds);
+        ++(this->ss);
+        if(this->ss == 60)
+        {
+            this->ss = 0;
+            ++(this->mm);
+            if(this->mm == 60)
+            {
+                this->mm = 0;
+                ++(this->hh);
+            }
+        }
+    }
+    
+    ulong get_total_seconds() { return this->total_seconds; }
+    
+    byte get_hours() { return this->hh; }
+    
+    byte get_minutes() { return this->mm; }
+    
+    byte get_seconds() { return this->ss; }
+};
+TimeClock timeClock;
+
+class DisplayMenu
+{
+private:
+    // indicates the screen number
+    byte screen_no;
+    
+    // indicates whether text was displayed to avoid displaying it at each update
+    byte is_text_displayed;
+    
+    void lcd_display(int col, int row, float value, int total_length, int n_decimals)
+    {
+        LCD.setCursor(col, row);
+        if (isnan(value))
+        {
+            LCD.print("ERR");
+        }
+        else
+        {
+            dtostrf(value, total_length, n_decimals, line);
+            LCD.print(line);
+        }
+    }
+public:
+    DisplayMenu()
+    {
+        this->screen_no = 1;
+        this->is_text_displayed = 0;
+    }
+
+    byte get_screen_no()
+    {
+        return this->screen_no;
+    }
+    
+    void next_screen()
+    {
+        if(++this->screen_no > MAX_LCD_DISPLAYS)
+        {
+            this->screen_no = 1;
+        }
+        this->is_text_displayed = 0;
+        LCD.clear();
+        this->display_screen();
+    }
+    
+    void display_screen()
+    {
+        switch(this->screen_no)
+        {
+            case 1: _display_screen_1(); break;
+            case 2: _display_screen_2(); break;
+            default: _display_error(); break;
+        }
+    }
+
+    void _display_error()
+    {
+        LCD.setCursor(0, 0);
+        LCD.print("There was an error!");
+        LCD.setCursor(0, 2);
+        LCD.print("Please check code!");
+    }
+    
+    void _display_screen_1()
+    {
+        if(this->is_text_displayed == 0)
+        {
+            this->is_text_displayed = 1;
+            LCD.setCursor(LCD_COL_TXT_SPEED, LCD_ROW_TXT_SPEED);
+            LCD.print("Speed:");
+        
+            LCD.setCursor(LCD_COL_TXT_TEMPERATURE, LCD_ROW_TXT_TEMPERATURE);
+            LCD.print("Temperature:");
+        
+//            LCD.createChar(0, symbol_celsius_degree);
+//            LCD.setCursor(LCD_COL_TXT_TEMPERATURE + 5, LCD_ROW_TXT_TEMPERATURE);
+//            LCD.write(byte(0));
+        
+            LCD.setCursor(LCD_COL_TXT_DISTANCE_CRT, LCD_ROW_TXT_DISTANCE_CRT);
+            LCD.print("Crt distance:");
+            
+            LCD.setCursor(LCD_COL_TXT_DISTANCE_TOTAL, LCD_ROW_TXT_DISTANCE_TOTAL);
+            LCD.print("Tot distance:");
+        }
+        lcd_display(LCD_COL_VAL_SPEED, LCD_ROW_VAL_SPEED, var_speed_kmh, 5, 2);
+        lcd_display(LCD_COL_VAL_DISTANCE_CRT, LCD_ROW_VAL_DISTANCE_CRT, var_distance_temp / 100000.0, 5, 2);
+        lcd_display(LCD_COL_VAL_DISTANCE_TOTAL, LCD_ROW_VAL_DISTANCE_TOTAL, var_distance_total / 100000.0, 6, 2);
+        lcd_display(LCD_COL_VAL_TEMPERATURE, LCD_ROW_VAL_TEMPERATURE, dht.readTemperature(), 5, 2);
+    }
+    
+    void _display_screen_2()
+    {
+        if(this->is_text_displayed == 0)
+        {
+            this->is_text_displayed = 1;
+            
+            LCD.setCursor(LCD_COL_TXT_TIME_ELAPSED, LCD_ROW_TXT_TIME_ELAPSED);
+            LCD.print("Elapsed:");
+            
+            LCD.setCursor(LCD_COL_TXT_MAX_SPEED, LCD_ROW_TXT_MAX_SPEED);
+            LCD.print("Max speed:");
+            
+            LCD.setCursor(LCD_COL_TXT_AVG_SPEED, LCD_ROW_TXT_AVG_SPEED);
+            LCD.print("Avg  speed:");
+        
+            LCD.setCursor(LCD_COL_TXT_AVG_PACE, LCD_ROW_TXT_AVG_PACE);
+            LCD.print("Time / km:");
+        }
+        
+        LCD.setCursor(LCD_COL_VAL_TIME_ELAPSED, LCD_ROW_VAL_TIME_ELAPSED);
+        sprintf(time_string, "% d:%02d:%02d", timeClock.get_hours(), timeClock.get_minutes(), timeClock.get_seconds());
+        LCD.print(time_string);
+
+        lcd_display(LCD_COL_VAL_MAX_SPEED, LCD_ROW_VAL_MAX_SPEED, var_max_speed_kmh, 5, 2);
+        lcd_display(LCD_COL_VAL_AVG_SPEED, LCD_ROW_VAL_AVG_SPEED, avgSpeed.compute_average(), 5, 2);
+
+        LCD.setCursor(LCD_COL_VAL_AVG_PACE, LCD_ROW_VAL_AVG_PACE);
+
+        int x = (int) var_avg_pace_skm;
+        int y = (int) (var_avg_pace_skm / 60);
+        int z = (int) (var_avg_pace_skm % 60);
+        sprintf(time_string, "%02dm%02ds", y, z);
+        LCD.print(time_string);
+        Serial.print("x="); Serial.print(x); Serial.print(" y="); Serial.print(y); Serial.print(" z="); Serial.println(z);
+    }
+};
+DisplayMenu menu;
 
 void setup()
 {
     LCD.init();
     LCD.clear();
     LCD.backlight();
-
-    LCD.setCursor(LCD_COL_TXT_SPEED, LCD_ROW_TXT_SPEED);
-    LCD.print("Speed(km/h):");
-
-    LCD.setCursor(LCD_COL_TXT_TEMP, LCD_ROW_TXT_TEMP);
-    LCD.print("Temp( C):");
-
-    LCD.createChar(0, symbol_celsius_degree);
-    LCD.setCursor(LCD_COL_TXT_TEMP + 5, LCD_ROW_TXT_TEMP);
-    LCD.write(byte(0));
-
-    LCD.setCursor(LCD_COL_TXT_DISTANCE_CRT, LCD_ROW_TXT_DISTANCE_CRT);
-    LCD.print("Crt dist(km):");
-    
-    LCD.setCursor(LCD_COL_TXT_DISTANCE_TOTAL, LCD_ROW_TXT_DISTANCE_TOTAL);
-    LCD.print("Tot dist(km):");
     
     dht.begin();
     Serial.begin(DEFAULT_BAUD_RATE);
@@ -272,11 +491,13 @@ void setup()
 
     var_distance_temp = mem.read_distance(MemoryHandler::MODE_CRT_DIST);
     var_distance_total = mem.read_distance(MemoryHandler::MODE_TOTAL_DIST);
-    
+        
     pulse_last_time_millis = millis();
     pulse_last_diff_millis = 0;
     pulse_count_invalids = 0;
     update_last_millis = millis();
+
+    menu.display_screen();
     
     delay(100);
 }
@@ -291,30 +512,50 @@ void loop()
         update_last_millis = update_current_millis;
         mem.write_distance(var_distance_temp, MemoryHandler::MODE_CRT_DIST);
         mem.write_distance(var_distance_total, MemoryHandler::MODE_TOTAL_DIST);
-        lcd_display(LCD_COL_VAL_SPEED, LCD_ROW_VAL_SPEED, var_speed_kmh, 5, 2); // (var_speed_kmh < 10) ? 3 : 4
-        lcd_display(LCD_COL_VAL_DISTANCE_CRT, LCD_ROW_VAL_DISTANCE_CRT, var_distance_temp / 100000.0, 5, 2);
-        lcd_display(LCD_COL_VAL_DISTANCE_TOTAL, LCD_ROW_VAL_DISTANCE_TOTAL, var_distance_total / 100000.0, 6, 2);
-        lcd_display(LCD_COL_VAL_TEMP, LCD_ROW_VAL_TEMP, dht.readTemperature(), 5, 2);
-        /*
-        Serial.print(var_rpm);
-        Serial.print(' ');
-        Serial.print(var_speed_kmh);
-        Serial.print(' ');
-        Serial.println(var_distance_temp);
-        */
+        
+        var_avg_pace_skm = (ulong)(timeClock.get_total_seconds() / ((ulong)var_distance_temp / 100000.0));
+        if(var_speed_kmh > 0.5) // for 2.35m as wheel diameter and CONST_ZERORIZE_SPEED_THRESHOLD_MS=3000ms, min speed is 3600000*0.00235/3000=2.82
+        {
+            timeClock.tick();
+            avgSpeed.add(var_speed_kmh);
+        }
+
+        menu.display_screen();
     }
 
-    if(digitalRead(RESET_DISTANCE_BUTTON_PIN) == HIGH) {
-        button_last_time = millis();
+    if(digitalRead(RESET_DISTANCE_BUTTON_PIN) == HIGH)
+    {
+        button_last_time_high = millis();
+        button_last_value = 0;
     }
-    else {
-        button_time_diff = millis() - button_last_time;
-        if(button_time_diff > RESET_CRT_DIST_TIMEOUT_MS) {
-            var_distance_temp = 0;
-            mem.write_distance(var_distance_temp, MemoryHandler::MODE_CRT_DIST);
-            if(button_time_diff > RESET_TOTAL_DIST_TIMEOUT_MS) {
+    else
+    {
+        button_time_diff = millis() - button_last_time_high;
+        if(button_time_diff > RESET_TOTAL_DIST_TIMEOUT_MS)
+        {
+            if(button_last_value != RESET_TOTAL_DIST_TIMEOUT_MS)
+            {
+                button_last_value = RESET_TOTAL_DIST_TIMEOUT_MS;
                 var_distance_total = 0;
                 mem.write_distance(var_distance_total, MemoryHandler::MODE_TOTAL_DIST);
+            }
+        }
+        else if(button_time_diff > RESET_CRT_DIST_TIMEOUT_MS)
+        {
+            if(button_last_value != RESET_CRT_DIST_TIMEOUT_MS)
+            {
+                button_last_value = RESET_CRT_DIST_TIMEOUT_MS;
+                var_distance_temp = 0;
+                timeClock.reset();
+                mem.write_distance(var_distance_temp, MemoryHandler::MODE_CRT_DIST);
+            }
+        }
+        else if(button_time_diff > DISPLAY_NEXT_SCREEN_MS)
+        {
+            if(button_last_value != DISPLAY_NEXT_SCREEN_MS)
+            {
+                button_last_value = DISPLAY_NEXT_SCREEN_MS;
+                menu.next_screen();
             }
         }
     }
@@ -331,26 +572,19 @@ void ISR_count_hall_pulses()
     pulse_current_millis = millis();
     pulse_diff_millis = pulse_current_millis - pulse_last_time_millis;
     diff_pulse_ratio = max(pulse_last_diff_millis, pulse_diff_millis) / min(pulse_last_diff_millis, pulse_diff_millis);
-//    Serial.print(pulse_current_millis);
-//    Serial.print(' ');
-//    Serial.print(pulse_diff_millis);
-//    Serial.print(' ');
     if(diff_pulse_ratio > PULSE_RATIO_THRESHOLD) // filter out
     {
         ++pulse_count_invalids;
-//        Serial.print(pulse_diff_millis);
         if(pulse_count_invalids == CONST_MAX_INVALID_PULSES)
         {
-//            Serial.print(" R ");
             pulse_count_invalids = 0; // reset this counter and reset the state
             pulse_last_diff_millis = 0; // shold have set it to zero, but above i might have division by zero
             pulse_last_time_millis = pulse_current_millis;
         }
-//        Serial.println(" -");
     }
     else // keep current value
     {
-        movingAverage.add_data(pulse_diff_millis);
+        movingAverage.add(pulse_diff_millis);
         pulse_count_invalids = 0;
         pulse_last_diff_millis = pulse_diff_millis;
         pulse_last_time_millis = pulse_current_millis;
@@ -358,36 +592,11 @@ void ISR_count_hall_pulses()
         var_distance_temp += WHEEL_LENGTH_CM;
         var_distance_total += WHEEL_LENGTH_CM;
         var_speed_kmh = CONST_SPEED_KMH / average_diff;
-//        movingAverage.show_data();
-//        Serial.println(average_diff);
-//        Serial.println(var_speed_kmh);
-        //var_rpm = 60000 / average_diff;
-        /*
-            Formula for speed:
-                average  ms ... 2.35 m
-                3600 000 ms ... x km/h !!!
-                x = speed(km/h) = (3 600 000 ms * 2.35 m) / average ms  / 1000 = 8460 / average
-            Formula for distance travelled:
-                add wheel length at each revolution
-            Formula for var_rpm:
-                average ms ... 1 rot
-                 60 000 ms ... x rot
-                 x = var_rpm = 60 000 ms * rot / average ms
-        */
-    }
-}
 
-void lcd_display(int col, int row, float value, int total_length, int n_decimals)
-{
-    LCD.setCursor(col, row);
-    if (isnan(value))
-    {
-        LCD.print("ERR");
-    }
-    else
-    {
-        dtostrf(value, total_length, n_decimals, line);
-        LCD.print(line);
+        if(var_speed_kmh > var_max_speed_kmh)
+        {
+            var_max_speed_kmh = var_speed_kmh;
+        }
     }
 }
 
